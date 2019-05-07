d24ceb29071ac727a8712fe5b20c5c1aefefdd12
zeee <zeinabrabie37@gmail.com>
2019-05-07 16:24:41 +0200

    Fix bugs in Execution Stage

--- src/execute_stage.vhd
+++ src/execute_stage.vhd
@@ -9,76 +9,71 @@ entity ExecuteStage is
         N : natural := 16
     );
     port(
-        Sel1        : in std_logic_vector(3 downto 0);
-        A1          : in std_logic_vector(N-1 downto 0);
-        B1          : in std_logic_vector(N-1 downto 0);
-        Imm1        : in std_logic_vector(N-1 downto 0);
-        F1          : out std_logic_vector(N-1 downto 0);
-        SetC1       : in std_logic;
-        ClrC1       : in std_logic;
-        UpdateFlag1 : in std_logic;
+        A1    : in std_logic_vector(N-1 downto 0);
+        B1    : in std_logic_vector(N-1 downto 0);
+        F1    : out std_logic_vector(N-1 downto 0);
+        Cw_1  : in std_logic_vector(2*N-1 downto 0);
 
-        Sel2        : in std_logic_vector(3 downto 0);
-        A2          : in std_logic_vector(N-1 downto 0);
-        B2          : in std_logic_vector(N-1 downto 0);
-        Imm2        : in std_logic_vector(N-1 downto 0);
-        F2          : out std_logic_vector(N-1 downto 0);
-        SetC2       : in std_logic;
-        ClrC2       : in std_logic;
-        UpdateFlag2 : in std_logic;
+        A2    : in std_logic_vector(N-1 downto 0);
+        B2    : in std_logic_vector(N-1 downto 0);
+        F2    : out std_logic_vector(N-1 downto 0);
+        Cw_2  : in std_logic_vector(2*N-1 downto 0);
 
-        clk         : in std_logic;
-        reset       : in std_logic;
-        Flags       : out std_logic_vector(N-1 downto 0)
+        clk   : in std_logic;
+        reset : in std_logic;
+        Flags : out std_logic_vector(N-1 downto 0)
     );
 end ExecuteStage;
 
 architecture structure of ExecuteStage is
     signal x1, x2 : std_logic_vector(N-1 downto 0) := (others => 'Z');
     signal y1, y2 : std_logic_vector(N-1 downto 0) := (others => 'Z');
-    signal select1, select2 : std_logic_vector(3 downto 0) := (others => 'Z');
+
     signal Cin1, Cin2 : std_logic := 'Z';
     signal result1, result2 : std_logic_vector(N-1 downto 0) := (others => 'Z');
     signal Carryout1, Carryout2 : std_logic := 'Z';
-    signal imm_value1, imm_value2 : std_logic_vector(N-1 downto 0) := (others => 'Z');
     
     signal flag_load : std_logic := 'Z';
     signal flag1,flag2 : std_logic_vector(2 downto 0) := (others => 'Z');
     signal flag_in,flag_out  : std_logic_vector(N-1 downto 0) := (others => '0');
+
+    signal ret_flag_load : std_logic := 'Z';
+    signal ret_flag_in, ret_flag_out : std_logic_vector(N-1 downto 0) := (others => '0');
+    
     begin
         alsu_inst1 : entity orthrus.alsu
             generic map (N => N)
             port map (
-                Sel => Sel1,
+                Sel => Cw_1(31 downto 28),
                 A => A1,
                 B => B1,
                 Cin => Cin1,
-                Imm => Imm1,
+                Imm => Cw_1(6 downto 3),
                 F => F1,
                 Cout => Carryout1,
                 Zero => flag1(0),
                 Negative => flag1(1)
             );
-        flag1(2) <= '1' when SetC1 = '1' else
-                    '0' when ClrC1 = '1' else
+        flag1(2) <= '1' when Cw_1(8) = '1' else
+                    '0' when Cw_1(7) = '1' else
                     Carryout1;
 
         alsu_inst2 : entity orthrus.alsu
             generic map (N => N)
             port map (
-                Sel => Sel2,
+                Sel => Cw_2(31 downto 28),
                 A => A2,
                 B => B2,
                 Cin => Cin2,
-                Imm => Imm2,
+                Imm => Cw_2(6 downto 3),
                 F => F2,
                 Cout => Carryout2,
                 Zero => flag2(0),
                 Negative => flag2(1)
             );
         
-        flag2(2) <= '1' when SetC2 = '1' else
-                    '0' when ClrC2 = '1' else
+        flag2(2) <= '1' when Cw_2(8) = '1' else
+                    '0' when Cw_2(7) = '1' else
                     Carryout2;
 
         FlagReg_inst : entity orthrus.Reg
@@ -88,12 +83,23 @@ architecture structure of ExecuteStage is
                 rst_data => (others=>'0'), load => flag_load, reset => reset
             );
 
-        flag_load <= UpdateFlag1 or UpdateFlag2;
-        flag_in(2 downto 0) <=  flag2 when (UpdateFlag1 = '1' and UpdateFlag2 = '1') else
-                                flag1 when UpdateFlag1 = '1' else
-                                flag2 when UpdateFlag2 = '1';
+        flag_load <= Cw_1(9) or Cw_2(9);
+        flag_in(2 downto 0) <=  ret_flag_out when Cw_2(13) = '1' else
+                                flag2 when Cw_2(9) = '1' else
+                                ret_flag_out when Cw_1(13) = '1' else
+                                flag1 when Cw_1(9) = '1';
         
-        Flags <= flag_out;
+
+        Flags <= flag_in; --try it with flag_in
+
+        TempFlagReg_inst : entity orthrus.Reg
+            generic map ( n => N )
+            port map (
+                clk => clk, d => ret_flag_in, q => ret_flag_out,
+                rst_data => (others=>'0'), load => ret_flag_load, reset => reset
+            );
+        ret_flag_load <= Cw_2(12) or Cw_1(12);
+        ret_flag_in <= flag_out; --not sure that it should be from flag_out not flag_in
 
     sync_state : process(clk)
     begin
